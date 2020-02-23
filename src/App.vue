fa389f48b4115584059704e473fd7cc3009c4618
Yishin26 <yishinlin@outlook.com>
2020-02-23 16:16:41 +0800

    Initial publish

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
+    <!--第三步:使用組件-->
+     <NavView> </NavView> 
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
   </div>
 </template>
 
 <script>
+//第一步:導入組件
+import NavView from './components/NavView'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  //第二步:註冊組件
   components: {
-    HelloWorld
+    HelloWorld,
+    NavView
   }
 }
 </script>
---
+++ src/components/NavView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='nav-view'>
+      hello-vue!
+  </div>
+</template>
+
+<script>
+export default {
+    name:'navView'
+
+}
+</script>
+
+<style>
+.nav-view{
+    font-size: 20pt;
+    color: blue
+}
+
+</style>
